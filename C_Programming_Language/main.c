f245644383b4baf2c89127393a039a333a7adab5
kyuhwankim <45233650+kyuhwankim@users.noreply.github.com>
2018-11-25 22:28:33 +0900

    chapter_1_2 finished

--- C_Programming_Language/main.c
+++ C_Programming_Language/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
@@ -11,8 +11,8 @@ int main()
     step = 20;
     fahr = lower;
     while(fahr <= upper){
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n",fahr,celsius);
+        celsius = (5 / 9.0) * (fahr - 32);
+        printf("%3.0f\ %6.1f\n",fahr,celsius);
         fahr = fahr + step;
     }
     return 0;
